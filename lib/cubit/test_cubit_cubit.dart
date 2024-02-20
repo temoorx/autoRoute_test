b7a20c0863b3c4de7bc250741aa4d15432932b09
temoorx <temoorx@gmail.com>
2024-02-20 17:56:05 +0500

    Refactor imports and layout in test_cubit_cubit.dart and home_page.dart

--- lib/cubit/test_cubit_cubit.dart
+++ lib/cubit/test_cubit_cubit.dart
@@ -1,4 +1,5 @@
-import 'package:bloc/bloc.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'test_cubit_state.dart';
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
-import 'routes/app_router.dart';
-
 @RoutePage()
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -12,24 +10,20 @@ class HomePage extends StatelessWidget {
     return Scaffold(
         body: SafeArea(
       child: Center(
-        child: Row(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Home Page'),
-                ElevatedButton(
-                    onPressed: () {
-                      context.router.pushNamed('/second-page/456');
-                    },
-                    child: const Text('Go to Second Page')),
-                ElevatedButton(
-                    onPressed: () {
-                      context.router.pushNamed('/second-page/456/inner');
-                    },
-                    child: const Text('Go to Second Inner Page')),
-              ],
-            ),
+            const Text('Home Page'),
+            ElevatedButton(
+                onPressed: () {
+                  context.router.pushNamed('/second-page/456');
+                },
+                child: const Text('Go to Second Page')),
+            ElevatedButton(
+                onPressed: () {
+                  context.router.pushNamed('/second-page/456/inner');
+                },
+                child: const Text('Go to Second Inner Page')),
           ],
         ),
       ),
